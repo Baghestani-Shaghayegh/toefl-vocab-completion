294356e33417fb4d63eb050c432f1a25a4503f08
baghestani shaghayegh <sara7space@gmail.com>
2026-05-10 12:09:07 +0900

    feat: update robots and sitemap configurations for improved SEO management

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,12 @@
 export default function robots() {
   return {
-    rules: { userAgent: '*', allow: '/' },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/terms', '/privacy', '/dashboard', '/practice', '/auth'],
+      },
+    ],
     sitemap: 'https://lexivo.io/sitemap.xml',
   }
 }
